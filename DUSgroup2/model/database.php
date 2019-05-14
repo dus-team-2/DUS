55ad882a0107f5cb7de99f1354572548855ac0ec
ziyi <ziyi.shen@durham.ac.uk>
2019-05-14 11:59:45 +0100

    'databaseBacked;calendarsAdded'

--- DUSgroup2/model/database.php
+++ DUSgroup2/model/database.php
@@ -1,16 +1,16 @@
 <?php
 	function make_database_connection(){
 		//school database
-		/*$db_host = 'mysql:host=mysql.dur.ac.uk';
+		$db_host = 'mysql:host=mysql.dur.ac.uk';
 		$db_name = 'dbname=Xdqrs89_SE2_DUS';
 		$db_user = 'dqrs89';
-		$db_pass = 'dqrs89ru34nner';*/
+		$db_pass = 'dqrs89ru34nner';
 		
 		//local database
-		$db_host = 'mysql:host=localhost';
+		/*$db_host = 'mysql:host=localhost';
 		$db_name = 'dbname=DUS';
 		$db_user = 'root';//change to your own username
-		$db_pass = '19960614';//change to your own password
+		$db_pass = '19960614';//change to your own password*/
 		
 		$pdo = new PDO($db_host.';'.$db_name, $db_user, $db_pass);
 		return $pdo;
