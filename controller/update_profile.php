d69a36a6ab57406ee72e8a05b4422ab8852cbc29
JoyceYanqiongZhang <50221665+JoyceYanqiongZhang@users.noreply.github.com>
2019-05-15 22:29:37 +0100

    Update update_profile.php

--- controller/update_profile.php
+++ controller/update_profile.php
@@ -7,7 +7,7 @@
  */
 require_once ('../model/common.php');
 
-$current_user_id = 1;
+$current_user_id = $_SESSION['id'];
 $current_user_all = get_by_from('*', 'id', 'user', $current_user_id);
 
 //print_r($current_user_email);
