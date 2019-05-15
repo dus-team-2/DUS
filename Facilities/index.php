c1e74981fdcd4d3acd390bdec90a0454bec34ec2
systemdev1216 <49998645+systemdev1216@users.noreply.github.com>
2019-05-15 16:13:08 +0100

    Add files via upload

--- Facilities/index.php
+++ Facilities/index.php
@@ -11,36 +11,56 @@
 <body>
 <div id="space"><br></div>
 <div id="container">
-<div id="wb_grahansports" style="position:absolute;left:208px;top:78px;width:833px;height:176px;z-index:1;">
-<img src="images/Maiden-Castle.jpg" id="grahansports" alt=""></div>
-<div id="wb_txtheaderfacilities" style="position:absolute;left:208px;top:30px;width:289px;height:52px;z-index:2;">
-<span style="color:#000000;font-family:Verdana;font-size:43px;">Our Facilities</span></div>
-<div id="wb_fitness" style="position:absolute;left:208px;top:265px;width:406px;height:100px;z-index:3;">
-<a href="https://www.teamdurham.com/facilities/durham/fitnesssuite/"><img src="images/FitnessSuite.jpg" id="fitness" alt=""></a></div>
-<div id="wb_physiotherapy" style="position:absolute;left:635px;top:265px;width:406px;height:100px;z-index:4;">
-<a href="https://www.teamdurham.com/facilities/durham/mcp/"><img src="images/MCPButton.jpg" id="physiotherapy" alt=""></a></div>
-<div id="wb_atheletics" style="position:absolute;left:209px;top:376px;width:187px;height:136px;z-index:5;">
-<a href="./atheletics_track.php"><img src="images/AthleticsTrack.jpg" id="atheletics" alt=""></a></div>
-<div id="wb_squash" style="position:absolute;left:427px;top:376px;width:187px;height:136px;z-index:6;">
-<a href="./squash.php"><img src="images/Squash-Courts.jpg" id="squash" alt=""></a></div>
-<div id="wb_tennis" style="position:absolute;left:635px;top:376px;width:187px;height:136px;z-index:7;">
-<a href="./tennis.php"><img src="images/Tennis.jpg" id="tennis" alt=""></a></div>
-<div id="wb_aerobics" style="position:absolute;left:209px;top:529px;width:188px;height:136px;z-index:8;">
-<a href="./areobics.php"><img src="images/aerobics.jpg" id="aerobics" alt=""></a></div>
-<div id="wb_rowing" style="position:absolute;left:427px;top:529px;width:187px;height:136px;z-index:9;">
-<a href="https://www.teamdurham.com/facilities/durham/rowingtank/"><img src="images/Rowing-Tank.jpg" id="rowing" alt=""></a></div>
-<div id="wb_otherfacilities" style="position:absolute;left:635px;top:529px;width:187px;height:136px;z-index:10;">
-<a href="https://www.teamdurham.com/facilities/durham/grasspitches/"><img src="images/Other-Facilities.jpg" id="otherfacilities" alt=""></a></div>
-<div id="wb_accessinfo" style="position:absolute;left:854px;top:376px;width:187px;height:58px;z-index:11;">
-<a href="https://www.accessable.co.uk/"><img src="images/Access-Information.jpg" id="accessinfo" alt=""></a></div>
-<div id="wb_openingtime" style="position:absolute;left:854px;top:454px;width:187px;height:58px;z-index:12;">
-<a href="https://www.teamdurham.com/about/facilities/durham/openingtimes/"><img src="images/Opening-Times.jpg" id="openingtime" alt=""></a></div>
-<div id="wb_cafe" style="position:absolute;left:854px;top:529px;width:187px;height:58px;z-index:13;">
-<a href="https://www.teamdurham.com/about/facilities/durham/catering/"><img src="images/Catering.jpg" id="cafe" alt=""></a></div>
-<div id="wb_contact" style="position:absolute;left:854px;top:607px;width:187px;height:58px;z-index:14;">
-<a href="https://www.teamdurham.com/about/facilities/durham/contactus/"><img src="images/Contact-Us.jpg" id="contact" alt=""></a></div>
-<div id="wb_queens" style="position:absolute;left:209px;top:685px;width:836px;height:209px;z-index:15;">
-<a href="https://www.teamdurham.com/queenscampus/"><img src="images/Queens-Campus.jpg" id="queens" alt=""></a></div>
+<div class="img_row_container">
+    <div id="wb_txtheaderfacilities" style="width: 100%">
+        <span style="color:#000000;font-family:Verdana;font-size:43px;">Our Facilities</span></div>
+</div>
+    <div class="img_row_container">
+        <div id="wb_grahansports" style="width: 100%" >
+        <img src="images/Maiden-Castle.jpg" id="grahansports" alt=""></div>
+<br>
+    <div id="wb_fitness" style="width: 50%; float: left">
+    <a href="https://www.teamdurham.com/facilities/durham/fitnesssuite/"><img src="images/FitnessSuite.jpg" id="fitness" alt=""></a></div>
+    <div id="wb_physiotherapy" style="width: 50%; float: left">
+    <a href="https://www.teamdurham.com/facilities/durham/mcp/"><img src="images/MCPButton.jpg" id="physiotherapy" alt=""></a></div>
+</div>
+<br>
+    <div class="img_row_container">
+        <div id="wb_atheletics" style="width: 25%; float: left">
+            <a href="./navFacilityDetails.php?fid=12344571"><img src="images/AthleticsTrack.jpg" id="atheletics" alt=""></a></div>
+        <div id="wb_squash" style="width: 25%; float: left">
+            <a href="./navFacilityDetails.php?fid=12344570"><img src="images/Squash-Courts.jpg" id="squash" alt=""></a></div>
+        <div id="wb_tennis" style="width: 25%; float: left">
+            <a href="./navFacilityDetails.php?fid=12344569"><img src="images/Tennis.jpg" id="tennis" alt=""></a></div>
+        <div class="small_img_containner">
+            <div id="wb_accessinfo" style="width: 100%; margin-top: 5%; margin-bottom: 5%">
+                <a href="https://www.accessable.co.uk/"><img src="images/Access-Information.jpg" id="accessinfo" alt=""></a></div>
+            <div id="wb_openingtime" style="width: 100%; margin-top: 5%; margin-bottom: 5%">
+                <a href="https://www.teamdurham.com/about/facilities/durham/openingtimes/"><img src="images/Opening-Times.jpg" id="openingtime" alt=""></a></div>
+        </div>
+    </div>
+
+<div class="img_row_container">
+    <div id="wb_aerobics" style="width: 25%; float: left">
+    <a href="./navFacilityDetails.php?fid=12344572"><img src="images/aerobics.jpg" id="aerobics" alt=""></a></div>
+    <div id="wb_rowing" style="width: 25%; float: left">
+    <a href="https://www.teamdurham.com/facilities/durham/rowingtank/"><img src="images/Rowing-Tank.jpg" id="rowing" alt=""></a></div>
+    <div id="wb_otherfacilities" style="width: 25%; float: left">
+    <a href="https://www.teamdurham.com/facilities/durham/grasspitches/"><img src="images/Other-Facilities.jpg" id="otherfacilities" alt=""></a></div>
+
+    <div class="small_img_containner">
+        <div id="wb_cafe" style="width: 100%; margin-top: 5%; margin-bottom: 5%">
+        <a href="https://www.teamdurham.com/about/facilities/durham/catering/"><img src="images/Catering.jpg" id="cafe" alt=""></a></div>
+        <div id="wb_contact" style="100%; margin-top: 5%; margin-bottom: 5%">
+        <a href="https://www.teamdurham.com/about/facilities/durham/contactus/"><img src="images/Contact-Us.jpg" id="contact" alt=""></a></div>
+    </div>
+</div>
+
+<div class="img_row_container">
+    <div id="wb_queens" style="width: 100%">
+    <a href="https://www.teamdurham.com/queenscampus/"><img src="images/Queens-Campus.jpg" id="queens" alt=""></a></div>
+</div>
+</div>
 
 
 </body>
