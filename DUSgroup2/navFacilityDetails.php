8396921dd028644c6521bb477170eee80837889e
systemdev1216 <49998645+systemdev1216@users.noreply.github.com>
2019-05-19 03:12:55 +0100

    Add files via upload

--- DUSgroup2/navFacilityDetails.php
+++ DUSgroup2/navFacilityDetails.php
@@ -1,8 +1,8 @@
 <?php
 include "header.php";
 ?>
-    <link href="../DUSport.css" rel="stylesheet">
-    <link href="../areobics.css" rel="stylesheet">
+    <link href="DUSport.css" rel="stylesheet">
+    <link href="areobics.css" rel="stylesheet">
 
 <?php
 include "header2.php";
@@ -17,7 +17,7 @@ include "header2.php";
         }
         // require "dbconfig.php";
         include "side.php";
-        require_once('../facility_details.php');
+        require_once('facility_details.php');
         ?>
     </div>
 
