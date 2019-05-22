b55c52a8f061277059bcede7618396d170ebb8cc
JoyceYanqiongZhang <yanqiong.zhang@durham.ca.uk>
2019-05-22 15:33:24 +0100

    delete facility page layout changed

--- DUSgroup2/navFacilitiesDelete.php
+++ DUSgroup2/navFacilitiesDelete.php
@@ -13,45 +13,51 @@ $statement = $pdo->query("SELECT id, facility.name FROM facility;");
 $row = $statement->fetchAll(PDO::FETCH_ASSOC);
 //print_r($row);
 ?>
+<div class="individual_content">
+    <div class="individual_second_layer">
+        <div class="span9">
+            <h2>Delete Facilities</h2>
+        </div>
 
-<div class="span9">
-    <h1>Delete Facilities</h1>
+        <!-- Functions to alert if a value is null -->
+        <script>
+            function adminFacilitiesDelete(){
+                if($("#adminFacilitiesDeleteId").val()==""){
+                    alert("Please select a facility.");
+                }else{
+                    var r=confirm("Are you sure to delete this facility?")
+                    if(r==false){
+                        return false;
+                    }else{
+
+                    }
+                }
+            }
+        </script>
+
+        <!-- Select a facility -->
+        <div class = "span9">
+            <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="post" id="adminFacilitiesDelete">
+                <div class="form-row">
+                    <label for="adminFacilitiesDeleteId"><p class="explain"> *Please select a facility for deleting: </p></label>
+                    <select class="form-control" name="adminFacilitiesDeleteId" id="adminFacilitiesDeleteId"/>
+                    <option value="">-- Please select --</option>
+                    <?php foreach($row as $element){
+                        echo "<option value='".$element['id']."'>".$element['name']."</option>";
+                    }
+                    ?>
+                    </select>
+                </div>
+                <br>
+                <div class = "button">
+                    <input type="submit" class="btn btn-primary" onclick="return adminFacilitiesDelete();" value="Submit" />
+                </div>
+            </form>
+        </div>
+
+    </div>
 </div>
 
-    <!-- Functions to alert if a value is null -->
-    <script>
-        function adminFacilitiesDelete(){
-            if($("#adminFacilitiesDeleteId").val()==""){
-                alert("Please select a facility.");
-            }else{
-                var r=confirm("Are you sure to delete this facility?")
-                if(r==false){
-                    return false;
-                }else{
-
-                }
-			}
-        }
-    </script>
-
-    <!-- Select a facility -->
-    <div class = "span9">
-        <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="post" id="adminFacilitiesDelete">
-            <p>
-                <label for="adminFacilitiesDeleteId">Please select a facility for deleting: </label>
-                <select name="adminFacilitiesDeleteId" id="adminFacilitiesDeleteId"/>
-                <option value="">-- Please select --</option>
-                <?php foreach($row as $element){
-                    echo "<option value='".$element['id']."'>".$element['name']."</option>";
-                }
-                ?>
-                </select>
-            </p>
-            <div class = "button">
-                <input type="submit" onclick="return adminFacilitiesDelete();" value="Submit" />
-            </div>
-        </form>
-    </div>
 
 <?php
 if($_SERVER["REQUEST_METHOD"] === "POST") {
